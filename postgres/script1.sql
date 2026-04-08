8eb75d98176c381aefe44096f91b96344cd660fb
alex <alex20log06.cir@gmail.com>
2026-04-08 16:47:00 +0200

    fix completo per infrastructure, considerare punto di partenza

--- postgres/script1.sql
+++ postgres/script1.sql
@@ -4,7 +4,12 @@ create table images
 	image_id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
 	title varchar(100) not null,
 	original_title varchar(100) not null,
-	year smallint not null,
+	"year" smallint not null,
 	place varchar(100) not null,
-	path varchar(255) not null
+	"path" varchar(255) not null
 );
+
+INSERT INTO images
+	(title, original_title, "year", place, "path")
+VALUES
+	('titolo','OG',1942,'nowhere','patthino');
